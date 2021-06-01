f6fa4e3d6818c05c554549679c3f585ed3c33184
mohammad-lababneh <mohammadlababneh96@gmail.com>
2021-06-01 12:42:38 +0300

    adding

--- js/app.js
+++ js/app.js
@@ -1,50 +1,55 @@
 let ul = document.getElementById('ul');
 
 let lovers = [];
-function foodlover (cusName,foodType){
+function Foodlovers(cusName, foodType) {
     this.cusName = cusName;
     this.foodType = foodType;
     let randomNum = [];
     settingItems()
-lovers.push(this);
+    lovers.push(this);
 }
 
-function settingItems(){
+function settingItems() {
     let order = JSON.stringify(lovers);
-    localStorage.setItem('key',order);
-    render ();
+    localStorage.setItem('key', order);
+
 
 }
-function getItems(){
+function getItems() {
     let strringOrder = localStorage.getItem('key');
     let normOrder = JSON.parse(strringOrder);
-if ( normOrder !== null ){
-    lovers = normOrder}
+    if (normOrder !== null) {
+        lovers = normOrder
+    }
+    render();
 }
 
 function getRandomArbitrary() {
     randomNum.push(Math.random() * (max - min) + min);
-  }
-
-let form = document.getElementById('form');
-form.addEventListener('submit',addOrder);
-
-function addOrder (event){
-    event.preventDefault();
-let cusName = event.target.cusName.value;
-let foodType = event.target.foodType.value;
- new foodlover (cusName,foodType);
- render ();
 }
 
-function render (){
-    ul.textContent ="";
- for(let i = 0; i < lovers.length; i++) {
-   let liEl = document.createElement('li');
-   liEl.textContent = `${lovers[i].cusName } ordered ${lovers[i].foodType }`
-   ul.appendChild(ilEl);
-    
-}}
+let form = document.getElementById('form');
+form.addEventListener('submit', addOrder);
+
+function addOrder(event) {
+    event.preventDefault();
+
+    let cusName = event.target.cusName.value;
+    let foodType = event.target.foodType.value;
+
+    new Foodlovers(cusName, foodType);
+    render();
+}
+
+function render() {
+    ul.textContent = "";
+    for (let i = 0; i < lovers.length; i++) {
+        let liEl = document.createElement('li');
+        liEl.textContent = `${lovers[i].cusName} ordered ${lovers[i].foodType}`
+        ul.appendChild(liEl);
+
+    }
+}
 
 
 getItems();
